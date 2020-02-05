d4ac03bf6f29c060d505b8d08ab2dcd5b0a7a232
Hayley Corson-Dosch <corsondosch@wisc.edu>
2020-02-05 17:40:05 -0600

    Week 4 Activity Completed
    Week 4 Activity Completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,150 @@
-
 // // Javascript script by Hayley Corson-Dosch
+
+//initialize function called when the script loads
+//Use the intitialize function to call the cities function
+function initialize(){
+	cities();
+  debugAjax();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+	//define an array of objects for cities and their population
+	var cityPop = [
+		{
+			city: 'Madison',
+			population: 233209
+		},
+		{
+			city: 'Milwaukee',
+			population: 594833
+		},
+		{
+			city: 'Green Bay',
+			population: 104057
+		},
+		{
+			city: 'Superior',
+			population: 27244
+		}
+	];
+
+	//append the table element to the div
+	$("#mydiv").append("<table>");
+
+	//append a header row to the table
+	$("table").append("<tr>");
+
+	//add the "City" and "Population" columns to the header row
+	$("tr").append("<th>City</th><th>Population</th>");
+
+	//loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
+
+		// call the addColumns and addEvents functions
+    addColumns(cityPop);
+    addEvents();
+};
+
+// define a function to add and populate a City Size column
+function addColumns(cityPop){
+		// Loop through the rows to add a new column and populate it
+    $('tr').each(function(i){
+
+    	if (i == 0){
+			// For the header row, add a new header called City Size
+    		$(this).append('<th>City Size</th>');
+    	} else {
+			// for the remaining rows...
+				// define a variable citySize
+    		var citySize;
+
+				// Use conditional statements to set the value of citySize,
+				// depending on the population of each city
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+
+    		} else if (cityPop[i-1].population < 500000){
+    			citySize = 'Medium';
+
+    		} else {
+    			citySize = 'Large';
+    		};
+
+				// Append the value of citySize to each row
+    		$(this).append('<td>' + citySize + '</td>');
+    	};
+    });
+};
+
+// define a function to add events to the page
+function addEvents(){
+
+// Define a mouseover function that randomly changes the
+// color of the text each time the user mouses over the table
+	$('table').mouseover(function(){
+	// Upon each mouseover...
+		//generate a random color
+		var color = "rgb(";
+
+		for (var i=0; i<3; i++){
+
+			var random = Math.round(Math.random() * 255);
+
+			color += random;
+
+			if (i<2){
+				color += ",";
+
+			} else {
+				color += ")";
+			};
+		};
+		// assign the color to the table style
+		$(this).css('color', color);
+	});
+
+// Define a clcik function that displays a pop-up
+// message each time the user clicks on the table
+	function clickme(){
+	// Define the pop-up alert to be displayed
+		alert('Hey, you clicked me!');
+	};
+	// Tell the function to display the alert each time
+	// the table is clicked
+	$('table').on('click', clickme);
+};
+
+
+//// WEEK 3 CODE BELOW
+// define the mydata variable globally
+var mydata;
+
+function debugCallback(response){
+  // define mydata variable
+  mydata = response;
+  // data can be accessed within the callback function
+  console.log('This is the data we requested: ', mydata);
+  // append the response as plain text to the mydiv block in the body of our html
+  $(mydiv).append('<br>GeoJSON data:<br>' + JSON.stringify(mydata));
+};
+
+function debugAjax(){
+  // Use the basic jQuery ajax method to request the json data using an AJAX request
+	$.ajax("data/MegaCities.geojson", {
+		dataType: "json",
+		success: debugCallback
+	});
+};
+
+// data cannot be accessed outside of the callback function (prints undefined)
+console.log('This is undefined: ', mydata)
+
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
